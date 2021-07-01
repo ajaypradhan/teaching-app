c568929445e470b3753d9d1dd9bfe4abc798951a
Ajay Pradhan <pradhanajay14@gmail.com>
2021-07-01 23:25:50 +0530

    chnges made in js file and ui conplete without canvas

--- script.js
+++ script.js
@@ -1,8 +1,6 @@
 const videoElement = document.querySelector("video");
 const whiteBG = document.querySelector('.white');
 const blackBG = document.querySelector('.black');
-const board = document.querySelector('.board');
-
 
 (async function () {
     let constraint = { video: true };
@@ -10,9 +8,4 @@ const board = document.querySelector('.board');
     videoElement.srcObject = mediaStream;
 })();
 
-blackBG.addEventListener('click',function(){
-    board.setAttribute("style", "background-color:black");
-})
-whiteBG.addEventListener('click',function(){
-    board.setAttribute("style", "background-color:white");
-})
+
