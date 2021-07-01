c8b10c577bed9c4ead1cb1ef751e1c27e6fabc65
Ajay Pradhan <pradhanajay14@gmail.com>
2021-07-01 21:39:21 +0530

    sheet color change function done

--- script.js
+++ script.js
@@ -1,6 +1,18 @@
-let videoElement = document.querySelector("video");
+const videoElement = document.querySelector("video");
+const whiteBG = document.querySelector('.white');
+const blackBG = document.querySelector('.black');
+const board = document.querySelector('.board');
+
+
 (async function () {
     let constraint = { video: true };
     let mediaStream = await navigator.mediaDevices.getUserMedia(constraint);
     videoElement.srcObject = mediaStream;
 })();
+
+blackBG.addEventListener('click',function(){
+    board.setAttribute("style", "background-color:black");
+})
+whiteBG.addEventListener('click',function(){
+    board.setAttribute("style", "background-color:white");
+})
