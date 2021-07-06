b691704ba21b381dfa5cfd5b042b460a290a0a26
Ajay Pradhan <pradhanajay14@gmail.com>
2021-07-07 01:36:56 +0530

    changes in ui animation, canvas added

--- script.js
+++ script.js
@@ -1,11 +1,20 @@
-const videoElement = document.querySelector("video");
+const videoElement = document.querySelector('video');
+const canvas = document.getElementById('myCanvas');
 const whiteBG = document.querySelector('.white');
 const blackBG = document.querySelector('.black');
 
+//start camera as soon a site open
 (async function () {
     let constraint = { video: true };
     let mediaStream = await navigator.mediaDevices.getUserMedia(constraint);
     videoElement.srcObject = mediaStream;
 })();
 
+// change background of canvas
+whiteBG.addEventListener('click', function () {
+    canvas.setAttribute('style', 'background-color:white');
+});
 
+blackBG.addEventListener('click', function () {
+    canvas.setAttribute('style', 'background-color:black');
+});
