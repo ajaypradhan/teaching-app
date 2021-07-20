d4d45114cbd01951f162abfed12b99dab125c390
Ajay Pradhan <pradhanajay14@gmail.com>
2021-07-21 00:05:07 +0530

    changes

--- js/add_new_sheet.js
+++
@@ -1,6 +0,0 @@
-const addPage = document.querySelector('#add-page');
-addPage.addEventListener('click', function () {
-    // alert('Add new page');
-    linesDB = []
-    ctx.clearRect(0, 0, canvas.width, canvas.height);
-});
