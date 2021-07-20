c6df6eb9a9ec1cfdac7a353686a973204621db55
Ajay Pradhan <pradhanajay14@gmail.com>
2021-07-20 11:08:20 +0530

    remove note ad highlighter

--- js/tools_option.js
+++ js/tools_option.js
@@ -1,14 +1,24 @@
 const pen = document.querySelector('#pen');
 const eraser = document.querySelector('#eraser');
+const highlighter = document.querySelector('#highlighter');
 const penOption = document.querySelector('.pen-option');
 const eraserOption = document.querySelector('.eraser-option');
+const highlighterOption = document.querySelector('.highlighter-option');
 
 pen.addEventListener('click', function () {
-    eraserOption.classList.remove('select');
     penOption.classList.toggle('select');
+    eraserOption.classList.remove('select');
+    highlighterOption.classList.remove('select');
 });
 
 eraser.addEventListener('click', function () {
-    penOption.classList.remove('select');
     eraserOption.classList.toggle('select');
+    penOption.classList.remove('select');
+    highlighterOption.classList.remove('select');
+});
+
+highlighter.addEventListener('click', function () {
+    highlighterOption.classList.toggle('select');
+    penOption.classList.remove('select');
+    eraserOption.classList.remove('select');
 });
